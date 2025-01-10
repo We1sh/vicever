4de1f8f168e477f441886388f7f03b4d430f8401
We1sh <90512906+We1sh@users.noreply.github.com>
2025-01-10 22:30:02 +0200

    Words counter added

--- home.py
+++ home.py
@@ -4,5 +4,7 @@ def view(request):
 
 def reverse(request):
     user_text=request.GET['usertext']
+    words=len(user_text.split())
     reversed_text=user_text[::-1]
-    return render(request,'reverse.html',{'usertext':user_text,'reversedtext':reversed_text})
+    return render(request,'reverse.html',{'usertext':user_text,'reversedtext':reversed_text,
+    'words':words})
